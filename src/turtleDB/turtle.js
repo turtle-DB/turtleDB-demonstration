2700a7cbd3e94cca2def3fa94bd07d7c6af30024
Steven Shen <steeve.shen@gmail.com>
2018-07-18 14:36:38 -0400

    implement metaStore & create w/ revs
    Co-authored-by: Max Appleton <maxiappleton@gmail.com>
    Co-authored-by: Andrew Houston-Floyd <andrew.houstonfloyd@gmail.com>

--- src/turtleDB/turtle.js
+++ src/turtleDB/turtle.js
@@ -9,12 +9,29 @@ class TurtleDB {
 
   create(data) {
     if (typeof data === 'object' && !Array.isArray(data)) {
-      data.id = uuidv4();
-      //revision ids
-      let newDoc = { key: data.id, data };
-      const rev = '1-' + md5(JSON.stringify(newDoc));
-      newDoc.data.rev = rev;
-      return this.idb._crud('create', newDoc);
+      let newDoc = Object.assign({}, data);
+
+      let _id;
+      if (!newDoc._id && newDoc._id !== 0) {
+        _id = uuidv4();
+      } else {
+        _id = newDoc._id;
+        delete newDoc._id;
+      }
+
+      //revision + id
+      const _rev = '1-' + md5(JSON.stringify(newDoc));
+      newDoc._id_rev = _id + '::' + _rev;
+
+      //insert into meta
+      let metaDoc = { _id, revisions: [_rev] };
+      return this.idb._crud(this.idb._meta, 'create', { data: metaDoc })
+        .then(() => {
+          return this.idb._crud(this.idb._store, 'create', { data: newDoc });
+        })
+        .catch(err => {
+          console.log("Create error:", err);
+        })
     } else {
       console.log('Please pass in a valid object.')
     }
@@ -24,12 +41,19 @@ class TurtleDB {
     return this.idb._crud('read', { key });
   }
 
+  // get document
+    //getDoc(ID)
+      //first go to meta store, get winningRevID
+      //concat ID - winningRevID
+      //go to _store index
+      //takes us to store data
+
   update(key, data) {
     return this.read(key).then((doc) => {
       let updatedDoc = Object.assign({}, doc, data);
-      const oldRev = parseInt(updatedDoc.rev.split('-')[0], 10);
+      const oldRev = parseInt(updatedDoc._rev.split('-')[0], 10);
       const newRev = `${oldRev + 1}-` + md5(updatedDoc);
-      updatedDoc.rev = newRev;
+      updatedDoc._rev = newRev;
       return this.idb._crud('create', { key, updatedDoc });
     })
   }
