b331e5ef9031b72ea16b2712b97022b76adb181d
Steven Shen <steeve.shen@gmail.com>
2018-07-23 16:16:01 -0400

    refactor developerAPI
    Co-authored-by: Andrew Houston-Floyd <andrew.houstonfloyd@gmail.com>

--- src/turtleDB/replicator.js
+++ src/turtleDB/replicator.js
@@ -27,7 +27,6 @@ class Replicator {
     .then(() => this.createNewSyncDocument()) //this.turtleSyncRecord
     .then(() => this.sendTurtleDocsAndSyncRecordToTortoise('/_bulk_docs'))
     .then(() => this.updateTurtleSyncHistory(this.turtleSyncRecord))
-    .then(res => console.log(res))
     .catch(err => console.log(err));
   }
 
