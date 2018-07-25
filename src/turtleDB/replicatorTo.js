aca656f763947c8697a66c6ad96d4663304c9f5c
Steven Shen <steeve.shen@gmail.com>
2018-07-25 11:48:41 -0400

    todo: remove _id from tortoise store
    Co-authored-by: Max Appleton <maxiappleton@gmail.com>
    Co-authored-by: Andrew Houston-Floyd <andrew.houstonfloyd@gmail.com>

--- src/turtleDB/replicatorTo.js
+++ src/turtleDB/replicatorTo.js
@@ -51,7 +51,7 @@ class ReplicatorTo {
   }
 
   getSourceHistoryDoc() {
-    return this.idb.command(this.idb.replicationHistoryTo, "READ_ALL", {})
+    return this.idb.command(this.idb._replicationHistoryTo, "READ_ALL", {})
     .then(syncRecords => this.sourceHistoryDoc = syncRecords[0])
   }
 
