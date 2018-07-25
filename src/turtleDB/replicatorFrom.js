7e9f699618964f2029fa5cc37749c6d126606cf5
houstonfloyd <andrew.houstonfloyd@gmail.com>
2018-07-24 21:57:30 -0400

    Finish tortoise -> turtle replication
    Co-authored-by: Steven Shen <steeve.shen@gmail.com>
    Co-authored-by: Max Appleton <maxiappleton@gmail.com>

--- src/turtleDB/replicatorFrom.js
+++ src/turtleDB/replicatorFrom.js
@@ -11,10 +11,9 @@ class ReplicatorFrom {
   replicate() {
     this.getLastTargetKey()
     .then(() => this.requestMetaDocs('/_source_meta_docs'))
-    .then((res) => {
-      const sourceMetaDocs = res.data;
-      this.revDiffs(sourceMetaDocs)
-    });
+    .then((res) => this.revDiffs(res.data))
+    .then(() => this.requestStoreDocs('/_source_store_docs'))
+    .then((res) => this.updateStoreAndReplicationHistory(res.data));
   }
 
   getTurtleID() {
@@ -41,77 +40,74 @@ class ReplicatorFrom {
 
   revDiffs(sourceMetaDocs) {
     const ids = sourceMetaDocs.map(doc => doc._id);
-    console.log('ids:', ids);
     return this.readMetaDocs(ids)
     .then(targetMetaDocs => {
       const missingRevs = this.findMissingRevs(sourceMetaDocs, targetMetaDocs);
-      console.log(missingRevs);
-    });
+      this.updateMetaDocs(missingRevs);
+      return missingRevs;
+    })
+    .then(metaDocs => {
+      this.revIds = metaDocs.map(doc => {
+        return doc._id + "::" + doc.revisions[0];
+      })
+    })
+    .catch(err => console.log(err));
+  }
+
+  updateMetaDocs(sourceMetaDocs) {
+    let promises = [];
+    sourceMetaDocs.forEach(doc => promises.push(this.idb.command(this.idb._meta, "UPDATE", { data: doc })))
+    return Promise.all(promises);
   }
 
   readMetaDocs(ids) {
     let promises = [];
     ids.forEach(_id => promises.push(this.idb.command(this.idb._meta, "READ", { _id })))
+    return Promise.all(promises).then(metadocs => metadocs.filter(doc => doc));
+  }
+
+  findMissingRevs(sourceMetaDocs, targetMetaDocs) {
+    const latestTargetDocRev = {};
+    targetMetaDocs.forEach(doc => {
+      latestTargetDocRev[doc._id] = doc.revisions[0];
+    })
+
+    return sourceMetaDocs.filter(doc => {
+      let targetRevId = latestTargetDocRev[doc._id];
+      if (targetRevId) {
+        if (targetRevId !== doc.revisions[0]) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    })
+  }
+
+  requestStoreDocs(path) {
+    return axios.post(this.targetUrl + path, { revIds: this.revIds });
+  }
+
+  updateStoreAndReplicationHistory(obj) {
+    const { docs, sourceSyncRecord } = obj;
+    this.updateDB(docs)
+    .then(() => this.updateSyncHistory(sourceSyncRecord));
+  }
+
+  updateDB(docs) {
+    console.log('docs are', docs);
+    let promises = [];
+    docs.forEach(doc => promises.push(this.idb.command(this.idb._store, "CREATE", { data: doc })))
     return Promise.all(promises);
   }
 
-  missingRevs(sourceMetaDocs, targetMetaDocs) {
-    //   const latestTargetDocRev = {};
-    //   targetMetaDocs.forEach(doc => {
-    //     latestTargetDocRev[doc._id] = doc.revisions[0];
-    //   })
-    //
-    //   return sourceMetaDocs.filter(doc => {
-    //     let targetRevId = latestTargetDocRev[doc._id];
-    //     if (targetRevId) {
-    //       if (targetRevId !== doc.revisions[0]) {
-    //         return true;
-    //       } else {
-    //         return false;
-    //       }
-    //     } else {
-    //       return true;
-    //     }
-    //   })
+  updateSyncHistory(sourceSyncRecord) {
+    this.idb.command(this.idb._replicationHistoryFrom, "UPDATE", { data: sourceSyncRecord });
   }
 }
-    // console.log(sourceMetaDocs);
-  //   const ids = sourceMetaDocs.map(doc => doc._id);
-  //
-  //   return mongoShell.readMetaDocs(ids)
-  //     .then(targetMetaDocs => {
-  //       const missingRevs = this.findMissingRevs(sourceMetaDocs, targetMetaDocs)
-  //       // should this be handled here?
-  //       mongoShell.updateMetaDocs(missingRevs);
-  //       return missingRevs;
-  //     })
-  //     .then(metaDocs => {
-  //       return metaDocs.map(doc => {
-  //         return doc._id + "::" + doc.revisions[0];
-  //       })
-  //     })
-  //     .catch(err => console.log(err));
-  // }
 
-  // findMissingRevs(sourceMetaDocs, targetMetaDocs) {
-  //   const latestTargetDocRev = {};
-  //   targetMetaDocs.forEach(doc => {
-  //     latestTargetDocRev[doc._id] = doc.revisions[0];
-  //   })
-  //
-  //   return sourceMetaDocs.filter(doc => {
-  //     let targetRevId = latestTargetDocRev[doc._id];
-  //     if (targetRevId) {
-  //       if (targetRevId !== doc.revisions[0]) {
-  //         return true;
-  //       } else {
-  //         return false;
-  //       }
-  //     } else {
-  //       return true;
-  //     }
-  //   })
-  // }
 
 export default ReplicatorFrom;
 
