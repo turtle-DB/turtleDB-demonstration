d5940ffa6c2f0c5cb131235c124abbd327b21352
Max Appleton <maxiappleton@gmail.com>
2018-08-01 15:28:13 -0500

    fix key range bug

--- src/components/TreeComponent/TreeComponent.js
+++ src/components/TreeComponent/TreeComponent.js
@@ -7,19 +7,15 @@ class TreeComponent extends React.Component {
     return (
       <div>
         <h4>Revision Tree Visualizer</h4>
-        <div className="row d-flex justify-content-between">
-          <div className="col-8">
-            <TreeDisplay
-              selectedTreeMetaDoc={this.props.selectedTreeMetaDoc}
-              handleTreeDocClick={this.props.handleTreeDocClick}
-            />
-          </div>
-          <div className="col-4">
-            <DocumentDisplay
-              selectedTreeDoc={this.props.selectedTreeDoc}
-              handlePickWinnerClick={this.props.handlePickWinnerClick}
-            />
-          </div>
+        <div className="d-flex justify-content-between">
+          <TreeDisplay
+            selectedTreeMetaDoc={this.props.selectedTreeMetaDoc}
+            handleTreeDocClick={this.props.handleTreeDocClick}
+          />
+          <DocumentDisplay
+            selectedTreeDoc={this.props.selectedTreeDoc}
+            handlePickWinnerClick={this.props.handlePickWinnerClick}
+          />
         </div>
       </div>
     );
--- src/components/TreeComponent/WinnerButton.js
+++ src/components/TreeComponent/WinnerButton.js
@@ -3,12 +3,14 @@ import React from 'react';
 class WinnerButton extends React.Component {
   render() {
     return (
-      <button
-        className="btn btn-primary"
-        onClick={this.props.handlePickWinnerClick}
-      >
-        Select Winning Rev
+      <div className="d-flex justify-content-center">
+        <button
+          className="btn btn-primary"
+          onClick={this.props.handlePickWinnerClick}
+        >
+          Select Winning Rev
       </button>
+      </div>
     );
   }
 }
