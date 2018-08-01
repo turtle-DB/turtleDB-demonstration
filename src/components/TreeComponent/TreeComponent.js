c85331edff89e526c38b12f3e39e86cdeec09e39
Steven Shen <steeve.shen@gmail.com>
2018-08-01 15:29:36 -0400

    resize JSON display

--- src/components/TreeComponent/TreeComponent.js
+++ src/components/TreeComponent/TreeComponent.js
@@ -6,16 +6,20 @@ class TreeComponent extends React.Component {
   render() {
     return (
       <div>
-        <h4 className="text-center">Revision Tree Visualizer</h4>
-        <div className="d-flex justify-content-between">
-          <TreeDisplay
-            selectedTreeMetaDoc={this.props.selectedTreeMetaDoc}
-            handleTreeDocClick={this.props.handleTreeDocClick}
-          />
-          <DocumentDisplay
-            selectedTreeDoc={this.props.selectedTreeDoc}
-            handlePickWinnerClick={this.props.handlePickWinnerClick}
-          />
+        <h4>Revision Tree Visualizer</h4>
+        <div className="row d-flex justify-content-between">
+          <div className="col-8">
+            <TreeDisplay
+              selectedTreeMetaDoc={this.props.selectedTreeMetaDoc}
+              handleTreeDocClick={this.props.handleTreeDocClick}
+            />
+          </div>
+          <div className="col-4">
+            <DocumentDisplay
+              selectedTreeDoc={this.props.selectedTreeDoc}
+              handlePickWinnerClick={this.props.handlePickWinnerClick}
+            />
+          </div>
         </div>
       </div>
     );
